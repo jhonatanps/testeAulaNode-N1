e338ea1ad84bb39d29633eb075fc770c0ea1dae8
Jhonatan Padua Soares <61254467+jhonatanps@users.noreply.github.com>
2024-03-01 20:00:05 -0300

    for

---
+++ for.js
@@ -0,0 +1,11 @@
+// sem o loop for
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+                                // mesmo que contador = contador + 1
+for(let contador = 0; contador <= 5; contador++){
+    console.log(contador);
+}
+
