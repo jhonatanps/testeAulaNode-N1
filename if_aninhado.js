6146c6cef3d1c46ef3afd12bbd2f05395f56a50c
Jhonatan Padua Soares <61254467+jhonatanps@users.noreply.github.com>
2024-03-01 19:32:56 -0300

    if_aninhado

---
+++ if_aninhado.js
@@ -0,0 +1,14 @@
+let nome = "";
+
+nome = "João";
+nome = "Manoel";
+
+if(nome != ""){
+    if(nome == "Jhonatan"){
+        console.log("Olá " + nome);
+    }else{
+        console.log("Olá " + nome);
+    }
+}else{
+    console.log("Por favor insira um nome");
+}
