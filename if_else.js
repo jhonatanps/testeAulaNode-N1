ef38ee600d88ea067122e67fa56c03f0e98ef7ea
Jhonatan Padua Soares <61254467+jhonatanps@users.noreply.github.com>
2024-03-01 19:14:34 -0300

    segundo commit

---
+++ if_else.js
@@ -0,0 +1,12 @@
+//comparar 2 nomes
+
+const primeiroNome = "Jhonatan";
+// para gerar o senão que e o mesmo que o else mudar o dado da variavel
+// segundoNome
+const segundoNome = "Jhonatan";
+
+if(primeiroNome == segundoNome){
+    console.log("Os dois nomes são iguais");
+}else{
+    console.log("Os nomes não são iguais");
+}
