24fb52855acf2aec7fd1f09276a6ebd1cc7d1719
Jhonatan Padua Soares <61254467+jhonatanps@users.noreply.github.com>
2024-03-01 19:24:13 -0300

    if_else_if

---
+++ if_else_if.js
@@ -0,0 +1,9 @@
+const nome = "JHONATAN";
+
+if(nome == "Jhonatan"){
+    console.log("O nome e igual " + nome + " = " + "Jhontan");
+}else if(nome == "jhonatan"){
+    console.log("O nome e igual " + nome + " = " + "jhontan");
+}else{
+    console.log("O nome " + nome + " ele não e igual a nem uma condição do codigo" );
+}
