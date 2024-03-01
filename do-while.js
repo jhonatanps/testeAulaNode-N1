3685ab56c2c113933d8211560f08ee7a0e9203d4
Jhonatan Padua Soares <61254467+jhonatanps@users.noreply.github.com>
2024-03-01 20:52:26 -0300

    do-while

---
+++ do-while.js
@@ -0,0 +1,23 @@
+let contador = 0;
+
+do {
+
+    console.log(contador);
+
+    //contador++
+    contador = contador + 1;
+
+
+} while(contador < 10);
+
+let contadorRe = 10
+
+do{
+
+    console.log(contadorRe);
+
+    contadorRe--;
+    // contadorRe = contadorRe - 1;
+
+
+} while(contadorRe >= 0);
