28404f0bc2778eed2ac8d9db6a97d4d0307804c8
Jhonatan Padua Soares <61254467+jhonatanps@users.noreply.github.com>
2024-03-01 21:09:12 -0300

    for_of

---
+++ for_of.js
@@ -0,0 +1,14 @@
+            // 0            1            2            3
+let nomes = ['Ysadora', 'Isabella', 'Priscilla', 'Jhonatan'];
+
+// (0) nome = 'Ysadora';
+// (1) nome = 'Isabella';
+// (2) nome = 'Priscilla';
+// (3) nome = 'Jhonatan';
+
+console.log(nomes);
+
+for(let nome of nomes){
+    console.log(nome);
+}
+
