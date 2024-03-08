2a5d6e5f22a846bed6cb7c359a054d810960ea5a
Jhonatan Padua Soares <61254467+jhonatanps@users.noreply.github.com>
2024-03-08 20:04:54 -0300

    modificar_array

---
+++ node-arrays/modificar_array.js
@@ -0,0 +1,42 @@
+// declarei o array
+let frutas = ['uva','morango','tomate'];
+
+// modifiquei o elemento 0
+frutas[0] = 'melao';
+
+console.log('Antes de inserir um dado ' + frutas.length);
+
+// adicionei um elemento ao array
+frutas.push('uva');
+
+console.log('Depois de inserir um dado ' + frutas.length);
+
+// produto final
+console.log(frutas);
+
+
+frutas.pop();
+
+console.log(frutas);
+
+console.log('Depois do pop ' + frutas.length);
+
+frutas.unshift('uva');
+
+console.log(frutas);
+
+// com constante
+const nome = ['jhonatan','padua'];
+
+nome.pop();
+
+nome.push('soare');
+
+nome[0] = 'Priscilla';
+
+console.log(nome);
+
+
+
+
+
