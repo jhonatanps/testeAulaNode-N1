bea08aab2ceabc2a0d0658b78a74b5fda31e80e2
Jhonatan Padua Soares <61254467+jhonatanps@users.noreply.github.com>
2024-03-08 19:17:45 -0300

    meu_primeiro_array

---
+++ node-arrays/declarando_array.js
@@ -0,0 +1,19 @@
+// declaração de array
+                //   0         1          2
+const meuArray = ['Cirilo', 'Rafael', 'Geovanna'];
+
+const meuArrayVariado = [1,'Jhonatan',1.2, ['jhonatan','padua'], {nome:'jhonatan'}];
+
+            //template string
+console.log(`Meu array varido  ${meuArrayVariado}`);
+
+            //conctenação simples
+console.log('Meu array simples ' + meuArray);
+
+// console.log(meuArray[0]);
+// console.log(meuArray[1]);
+// console.log(meuArray[2]);
+
+for(let i = 0; i < meuArray.length; i++){
+    console.log(meuArray[i]);
+}
