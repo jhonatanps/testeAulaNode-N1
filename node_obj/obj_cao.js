b3f380cbdcfa05c8141ef9dafead19383498a9c3
Jhonatan Padua Soares <61254467+jhonatanps@users.noreply.github.com>
2024-03-08 21:31:10 -0300

    obj_cao

---
+++ node_obj/obj_cao.js
@@ -0,0 +1,22 @@
+
+let cao = {
+    nome:'lulu',
+    idade:5,
+    raca:'caramelo'
+}
+
+cao.dono = 'Cirilo';
+
+console.log(cao);
+
+cao.dono = 'Thalles';
+
+console.log(cao);
+
+delete cao.dono;
+
+console.log(cao);
+
+
+
+
